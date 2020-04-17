6af0ac49cc2ed20b4d0014e0ecd2539ea1eeaf9d
Veronica Eulenberg <veronica.eulenberg@gmail.com>
2020-04-17 14:15:49 -0600

    maybe fix deploy

--- pages.js
+++ pages.js
@@ -1,3 +1,10 @@
 var ghpages = require('gh-pages');
+var homepage = require('./package.json').homepage
 
-ghpages.publish('build', function(err) {});
+ghpages.publish('build', function(error) {
+	if (err) {
+		console.log("ERROR while publishing to gh-pages:", error);
+	} else {
+		console.log("Site deployed at:", homepage);
+	}
+});
